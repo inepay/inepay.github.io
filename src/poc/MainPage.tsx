9daa5f75396b2a765e81d3222fdbfe00499caf74
Koustubh Desai <koustubh.desai@publicissapient.com>
2024-03-20 15:00:56 +0530

    feat(camera): facemesh implemented

--- src/poc/MainPage.tsx
+++ src/poc/MainPage.tsx
@@ -1,12 +1,73 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import DetectButton from './DetectButton';
 import CaptureButton from './CaptureButton';
+import * as tf from '@tensorflow/tfjs'
+import * as facemesh from '@tensorflow-models/facemesh'
+import Webcam from 'react-webcam';
+import { drawMesh } from "../utils/triangulation";
+
+async function main() {
+  await tf.setBackend('webgl'); // Use WebGL backend
+  // Load your model and perform operations
+}
+
+main();
 
 const MainPage: React.FC = () => {
   const [stream, setStream] = useState<MediaStream | null>(null);
   const [isVideoOn, setIsVideoOn] = useState(false);
   const [isFrontCamera, setIsFrontCamera] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+   // Load posenet
+// Load facemesh
+const runFacemesh = async () => {
+  const net = await facemesh.load();
+  setInterval(() => {
+    detect(net);
+  }, 10);
+};
+
+const detect = async (net: facemesh.FaceMesh) => {
+  if (
+    webcamRef.current &&
+    webcamRef.current.video &&
+    webcamRef.current.video.readyState === 4
+  ) {
+    // Get Video Properties
+    const video = webcamRef.current.video!;
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+
+    // Set video width
+    video.width = videoWidth;
+    video.height = videoHeight;
+
+    // Set canvas width
+    if (canvasRef.current) {
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+    }
+
+    // Make Detections
+    const face = await net.estimateFaces(video);
+    //console.log(face);
+
+    // Get canvas context
+    const ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
+    }
+  }
+};
+
+useEffect(() => {
+  runFacemesh();
+}, []);
 
   const handleDetectButtonClick = () => {
     // Handle navigation to the view for detecting car seats
@@ -70,16 +131,44 @@ const MainPage: React.FC = () => {
       <DetectButton onClick={handleDetectButtonClick} />
       <CaptureButton onClick={handleCaptureButtonClick} />
       {isVideoOn && (
-        <div>
-          <video ref={videoRef} width="100%" height="auto" autoPlay />
+        <div className="App-header">
+          {/* <video ref={videoRef} width="100%" height="auto" autoPlay /> */}
           <button onClick={stopCamera}>Stop Camera</button>
           <button onClick={toggleCamera}>Toggle Camera</button>
+          <Webcam
+          ref={webcamRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
         </div>
       )}
       {!isVideoOn && (
         <div>
           <button onClick={startCamera}>Start Camera</button>
-          <input type="file" accept="image/*" onChange={handleFileInputChange} />
         </div>
       )}
     </div>
