d8f4d4851998e9fff1960627ee2b8948a630950a
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-27 21:59:09 +0530

    Feat/working code (#4)
    A working version in place?

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ModelProvider } from './poc/ModelContext'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ModelProvider>
+      <App />
+    </ModelProvider>
   </React.StrictMode>
 );
 
