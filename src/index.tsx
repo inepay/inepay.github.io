fa18c516ba670628e02286a3a108435336b32657
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-30 14:38:30 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ModelProvider } from './poc/ModelContext'
+//import { ModelProvider } from './poc/ModelContext'
+import VideoCanvasProvider from './components/templates/VideoCanvasProvider';
+import HolisticDrawing from './components/organisms/HolisticDrawing';
+import Scene from './components/molecules/Scene';
+import HatModel from './components/organisms/HatModel';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ModelProvider>
+    {/* <ModelProvider>
       <App />
-    </ModelProvider>
+    </ModelProvider> */}
+    <VideoCanvasProvider>
+    {({ videoRef, canvasRef, subscribe, unsubscribe }) => (
+        <div className="holistic-container">
+          <video ref={videoRef} className="input_video" autoPlay muted playsInline></video>
+          <canvas ref={canvasRef} className="output_canvas" width={640} height={480}></canvas>
+          <Scene className="three-canvas" style={{ width: '640px', height: '480px', border: '1px solid black' }}>
+            <HatModel />
+          </Scene>
+          <HolisticDrawing videoRef={videoRef} canvasRef={canvasRef} subscribe={subscribe} unsubscribe={unsubscribe} />
+        </div>
+      )}
+    </VideoCanvasProvider>
   </React.StrictMode>
 );
 
