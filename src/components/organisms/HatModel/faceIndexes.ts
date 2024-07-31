731e09ef0366826b47618bab1069833f0aed7a74
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-31 17:20:51 +0530

    Create faceIndexes.ts

---
+++ src/components/organisms/HatModel/faceIndexes.ts
@@ -0,0 +1,71 @@
+const faceLandmarkIndices = {
+    // Right Eye
+    rightEye: {
+      leftEyeOuter: 33,
+      rightEyeInner: 160,
+      rightEyeOuter: 158,
+      rightEyeTop: 159,
+      rightEyeBottom: 145
+    },
+    
+    // Left Eye
+    leftEye: {
+      leftEyeInner: 133,
+      leftEyeOuter: 163,
+      leftEyeTop: 144,
+      leftEyeBottom: 145,
+      leftEyeTopOuter: 246
+    },
+    
+    // Eyebrows
+    eyebrows: {
+      rightEyebrowOuter: 70,
+      rightEyebrowInner: 72,
+      leftEyebrowOuter: 105,
+      leftEyebrowInner: 107
+    },
+    
+    // Nose
+    nose: {
+      noseTip: 1,
+      noseBridge: 2,
+      noseBottomLeft: 4,
+      noseBottomRight: 6
+    },
+    
+    // Mouth
+    mouth: {
+      mouthLeftCorner: 13,
+      mouthRightCorner: 14,
+      mouthTopCenter: 57,
+      mouthBottomCenter: 287
+    },
+    
+    // Face Outline
+    faceOutline: {
+      faceOutlineBottom: 0,
+      faceOutlineTop: 8,
+      faceOutlineLeft: 10,
+      faceOutlineRight: 11
+    },
+    
+    // Jawline
+    jawline: {
+      jawlineLeft: 1,
+      jawlineCenter: 2,
+      jawlineRight: 3
+    },
+    
+    // Chin
+    chin: {
+      chinLeft: 152,
+      chinRight: 154,
+      chinCenter: 157
+    },
+    
+    // Face Center
+    faceCenter: 5
+  };
+
+  export default faceLandmarkIndices
+  
