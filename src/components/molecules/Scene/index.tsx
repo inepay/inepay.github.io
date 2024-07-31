145989f6ebacb9f6fc6100bcb033f33b4a36a1ae
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-31 17:21:15 +0530

    Update index.tsx

--- src/components/molecules/Scene/index.tsx
+++ src/components/molecules/Scene/index.tsx
@@ -1,15 +1,49 @@
 import React, {useState, useEffect, useRef} from 'react';
-import { Canvas, CanvasProps } from '@react-three/fiber';
+import { Canvas, CanvasProps, useThree } from '@react-three/fiber';
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
-import { Vector3 } from 'three';
+import { Vector3, VideoTexture, PlaneGeometry, MeshBasicMaterial, Mesh } from 'three';
+
 import { PerspectiveCamera as PerspectiveCameraType } from 'three';
+import HatModel from '../../organisms/HatModel';
+
+interface VideoMeshProps {
+  videoRef: React.RefObject<HTMLVideoElement>;
+}
+
+const VideoMesh: React.FC<VideoMeshProps> = ({ videoRef }) => {
+  const [videoTexture, setVideoTexture] = useState<VideoTexture | null>(null);
+  const { scene } = useThree();
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const newVideoTexture = new VideoTexture(videoRef.current);
+      setVideoTexture(newVideoTexture);
+      scene.background = newVideoTexture; // Optionally set as background
+    }
+  }, [videoRef, videoRef.current, scene]);
+
+  if (!videoTexture) {
+    return null; // Return null if video texture is not available
+  }
+
+  return (
+    <mesh position={[0, 0, 0]}>
+      <planeGeometry args={[5, 5]} />
+      <meshBasicMaterial map={videoTexture} toneMapped={false} />
+    </mesh>
+  );
+};
+
+
 interface SceneProps extends CanvasProps {
   // Add any additional props for the scene
+  videoRef: React.RefObject<HTMLVideoElement>;
 }
 
 const Scene: React.FC<SceneProps> = (props) => {
   const [zoom, setZoom] = useState<number>(0.6); // Default zoom level
   const cameraRef = useRef<PerspectiveCameraType>(null);  // Specify the type for the ref
+  const videoRef = useRef<HTMLVideoElement>(null);
   const [directionalLightIntensity, setDirectionalLightIntensity] = useState<number>(1); // Added state
   const [directionalLightPosition, setDirectionalLightPosition] = useState<[number, number, number]>([10, 10, 10]); // Added position
   useEffect(() => {
@@ -18,6 +52,7 @@ const Scene: React.FC<SceneProps> = (props) => {
       cameraRef.current.lookAt(new Vector3(0, 0, 0));
     }
   }, [cameraRef]);
+  
   return (
     <Canvas {...props}>
       {/* Add your scene setup here */}
@@ -41,6 +76,7 @@ const Scene: React.FC<SceneProps> = (props) => {
           far={1000}             // Far Clipping Plane
         />
         <OrbitControls />
+        {/* <VideoMesh videoRef={videoRef} />  Include the video mesh here */}
       {props.children}
       <axesHelper args={[5]} />
       //Debug Box
