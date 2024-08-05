cd02a37a6672118396cc5ddbc9b3f286621eeacc
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-08-05 16:51:08 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import WasmProvider from './components/templates/WakaCode';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import { ModelProvider } from './poc/ModelContext'
-import VideoCanvasProvider from './components/templates/VideoCanvasProvider';
-import HolisticDrawing from './components/organisms/HolisticDrawing';
+//import VideoCanvasProvider from './components/templates/VideoCanvasProvider';
+//import HolisticDrawing from './components/organisms/HolisticDrawing';
 import Scene from './components/molecules/Scene';
 import HatModel from './components/organisms/HatModel';
+
+import VideoCanvasProvider from './components/templates/VideoCanvasProviderFaceMesh';
+import HolisticDrawing from './components/organisms/FaceMeshDrawing';
  
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,18 +21,20 @@ root.render(
     {/* <ModelProvider>
       <App />
     </ModelProvider> */}
-    <VideoCanvasProvider>
-    {({ videoRef, canvasRef, subscribe, unsubscribe }) => (
-        <div className="holistic-container">
-          <video ref={videoRef} className="input_video" autoPlay muted playsInline></video>
-          <canvas ref={canvasRef} className="output_canvas" width={640} height={480}></canvas>
-          <Scene className="three-canvas" videoRef={videoRef} style={{ width: '640px', height: '480px', border: '1px solid black' }}>
-            <HatModel videoRef={videoRef} subscribe={subscribe} unsubscribe={unsubscribe} />
-          </Scene>
-          <HolisticDrawing videoRef={videoRef} canvasRef={canvasRef} subscribe={subscribe} unsubscribe={unsubscribe} />
-        </div>
-      )}
-    </VideoCanvasProvider>
+    <WasmProvider>
+      <VideoCanvasProvider>
+      {({ videoRef, canvasRef, subscribe, unsubscribe }) => (
+          <div className="holistic-container">
+            <video ref={videoRef} className="input_video" autoPlay muted playsInline></video>
+            <canvas ref={canvasRef} className="output_canvas" width={640} height={480}></canvas>
+            <Scene className="three-canvas" style={{ width: '640px', height: '480px', border: '1px solid black' }}>
+              <HatModel videoRef={videoRef} subscribe={subscribe} unsubscribe={unsubscribe} />
+            </Scene>
+            <HolisticDrawing videoRef={videoRef} canvasRef={canvasRef} subscribe={subscribe} unsubscribe={unsubscribe} />
+          </div>
+        )}
+      </VideoCanvasProvider>
+    </WasmProvider>
   </React.StrictMode>
 );
 
