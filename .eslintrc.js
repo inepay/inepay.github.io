63e4ff634bde1c81da4dea4fdee93532134d85b9
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-08-15 13:22:54 +0530

    Update .eslintrc.js [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
- // Code generated via "Slingshot" 
- module.exports = {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "react/jsx-no-comment-textnodes": "off"
-    }
-  };
+// Code generated via "Slingshot"
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react/jsx-no-comment-textnodes": "off"
+  }
+};
